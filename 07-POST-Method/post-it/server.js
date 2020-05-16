4378141cec49ca2ce259a5a46021f46ccb6f1bc0
Casey Carroll <caseydean8@gmail.com>
2020-05-16 14:52:00 -0400

    Server.js renders index page

--- 07-POST-Method/post-it/server.js
+++ 07-POST-Method/post-it/server.js
@@ -26,16 +26,3 @@ function handleRequest(req, res) {
 server.listen(PORT, function () {
   console.log("Server is listening on PORT: " + PORT);
 });
-
-// four routes:
-//   * Home
-//   * Favorite Foods
-//   * Favorite Movies
-//   * Favorite CSS Frameworks
-// * Each route should be triggered by a different URL.
-// * Each route should display an HTML page listing your favorite three things of each.
-// * Be sure to use `fs` to serve your HTML files.
-
-// ## Bonuses
-// * Have your home page have links to all of your other pages.
-// * DRY up your code by only having a single `readFile`
